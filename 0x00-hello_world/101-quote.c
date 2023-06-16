5b89714762183f8414039908824a1ef871112922
Antonymaged <magedantony1@gmail.com>
2023-06-15 19:37:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char p[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write p(1.q.59);
 	return (1);
 }
