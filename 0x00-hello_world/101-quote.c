4e707c2f0a11ed7c7e1727e87e1e0f28823a65cf
Antonymaged <magedantony1@gmail.com>
2023-06-15 19:28:24 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	return (1);
+}
