9acec2e600563a5e7766d4e1873098a8ebcea03a
Antonymaged <magedantony1@gmail.com>
2023-06-19 14:28:25 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,26 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-int c = 0, i = 0, n[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-while (c < 10)
+int i = 0, j = 1;
+while (i < 100)
 {
-putchar(n[c] + '0');
-putchar(n[i] + '0');
+while (j < 100)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-i = (c == 0) ? 1 : (c == 1) ? 2 : (c == 2) ? 3
-: (c == 3) ? 4 : (c == 4) ? 5 : (c == 5) ? 6 :
-(c == 6) ? 7 : (c == 7) ? 8 : (c == 8) ? 9 : 9;
-while (i < 10)
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 98 || j != 99)
 {
-putchar(n[c] + '0');
-putchar(n[i] + '0');
 putchar(',');
 putchar(' ');
+}
+j++;
+}
 i++;
+j = i + 1;
 }
-c++;
-}
+putchar('\n');
 return (0);
 }
