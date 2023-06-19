17cfb9a2665d286dcb571541d0288af3b32fb584
Antonymaged <magedantony1@gmail.com>
2023-06-19 10:59:18 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,11 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-int c = 0;
-char n[] = "0123456789";
-while (c < 10)
+int n = 0;
+while (n < 9)
 {
-putchar(n[c]);
-c++;
+putchar(n);
+n++;
 }
 putchar('\n');
 return (0);
