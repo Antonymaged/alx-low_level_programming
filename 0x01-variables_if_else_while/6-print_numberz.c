f34c257406c298d0199d6ca54e192b631b5bf0cc
Antonymaged <magedantony1@gmail.com>
2023-06-19 10:53:34 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int c;
+int n[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+while (c < 10)
+{
+putchar(n[c]);
+c++;
+}
+putchar('\n');
+return (0);
+}
