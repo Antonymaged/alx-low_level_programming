52b0e1b38760e78fd0072d10d42833fec015decf
Antonymaged <magedantony1@gmail.com>
2023-06-19 11:29:48 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,26 +9,21 @@
 int main(void)
 {
   int i, j, k;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 720; i++)
   {
-    for (j = i + 1; j < 10; j++)
+    j = (i / 100) % 10;
+    k = (i / 10) % 10;
+    if (j < k && k < (i % 10))
     {
-      for (k = j + 1; k < 10; k++)
+      putchar(j + '0');
+      putchar(k + '0');
+      putchar(i % 10 + '0');
+      if (i < 719)
       {
-        putchar(i + '0');
-        putchar(',');
-        putchar(' ');
-        putchar(j + '0');
-        putchar(',');
-        putchar(' ');
-        putchar(k + '0');
-        if (i == 7 && j == 8 && k == 9)
-          break;
-        putchar(',');
-        putchar(' ');
+        putchar(' ,');
       }
     }
   }
   putchar('\n');
-  return 0;
+  return (0);
 }
