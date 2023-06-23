d3507086c875b2889850f5bb995d95e8b3d70cca
Antonymaged <magedantony1@gmail.com>
2023-06-22 19:34:29 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - print more numbers
+ */
+void more_numbers(void)
+{
+int c = 0;
+char i;
+while (c < 10)
+{
+for (i = '0'; i < '15'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+c++;
+}
+}
