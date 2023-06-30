559572871bf53aa18f9d668f180c1c0cd3e15e6f
Antonymaged <magedantony1@gmail.com>
2023-06-29 18:46:13 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _islower - did i mention that i hate betty
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
