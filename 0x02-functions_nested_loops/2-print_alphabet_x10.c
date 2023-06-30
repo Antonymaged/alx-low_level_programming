2ec19a4b4c25da1a8361765273d194301410c44c
Antonymaged <magedantony1@gmail.com>
2023-06-29 18:36:33 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - i hate betyyyyyyyyyyyy
+ */
+void print_alphabet_x10(void)
+{
+char a;
+int b = 0;
+while (b < 10)
+{
+a = 'a';
+while (a <= 'z')
+{
+putchar(a);
+a++
+}
+putchar('\n');
+b++;
+}
+}
