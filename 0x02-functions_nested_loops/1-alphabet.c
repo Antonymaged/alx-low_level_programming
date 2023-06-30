fc4127764ccafd2ba8a200274e54d8c9b150a2f0
Antonymaged <magedantony1@gmail.com>
2023-06-29 18:23:55 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,16 +11,3 @@ a++;
 }
 putchar('\n');
 }
-
-#include "main.h"
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
