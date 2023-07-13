81604ce886a453e92f193a406e27293c254b6208
Antonymaged <magedantony1@gmail.com>
2023-07-13 14:31:22 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - assign number of bytes into a variable
+ * @b : the nuber of bytes
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+malloc(b);
+}
