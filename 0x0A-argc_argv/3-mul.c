b4d1dbed23a41538e504845c2383e38410e7e590
Antonymaged <magedantony1@gmail.com>
2023-07-15 19:52:18 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,23 +4,23 @@
  * main - the function to multiply 2 numbers
  * @argc: number of arguments
  * @argv: values of arguments
- * @x: value of the first num
- * @y: value of the second num
- * @z: value of the therd num
  *
  * Return: Always(0) (Success)
  */
 int main(int argc, char *argv[])
 {
-int x, y, z;
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-z = x * y;
-printf("%d\n", z);
-return (0);
 if (argc < 3 || argc > 3)
 {
 printf("Error\n");
-return (1);
+exit (1);
+}
+else
+{
+int x, y, z;
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+z = x *y;
+printf("%d\n", z);
+return (0);
 }
 }
