69a480bb78c2656a363fc356d3b54f57306fbcde
Antonymaged <magedantony1@gmail.com>
2023-07-18 21:32:01 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,17 +10,17 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *ptr;
-ptr = malloc(sizeof(char)*size);
+ptr = malloc(sizeof(char) * size);
 if (size == 0 || ptr == NULL)
 {
 return (NULL);
 }
 else
 {
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 ptr[i] = c;
 }
 }
-return(ptr);
+return (ptr);
 }
