5d9ee310f879e4371d3b7a34832605cc6342f73d
Antonymaged <magedantony1@gmail.com>
2023-07-18 19:59:31 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -44,7 +44,7 @@ return (dest);
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
- * @owner: owner of the dog	
+ * @owner: owner of the dog
  *
  * Return: pointer to the new dog (Success), NULL otherwise
  */
